489dd5baca3d174984455035f89f4b98ec93045c
Mick Ohtake <mikado98765@gmail.com>
2023-04-14 17:20:10 -0500

    Adding pseudocode for shot sim

--- shot_sim/simulate_shots.py
+++ shot_sim/simulate_shots.py
@@ -3,15 +3,51 @@ import pymunk.pygame_util
 from typing import List
 import pygame
 import time
+ 
+  
+SHOT_ID_COUNTER = 0
+
+class Ball:
+    def __init__(self, team, label, loc):
+        self.team = team
+        self.label = label
+        self.loc = loc
+
+class Table:
+    def __init__(self, balls: List[Ball]):
+        self.balls = balls # Array of class Ball
+
+class Shot: # only created after ball has been made
+    def __init__(self, table: Table, angle, strength, difficulty):
+        self.angle = angle
+        self.strength = strength
+        self.difficulty = difficulty
+        self.table = table # initial state (before shot is taken)
+        
+        global SHOT_ID_COUNTER
+        self.shot_id = SHOT_ID_COUNTER
+        SHOT_ID_COUNTER += 1
+
+class ShotsTaken:
+    def __init__(self, previous_shots: List[Shot]):
+        self.previous_shots = previous_shots
+
+class Node:
+    def __init__(self, shot: Shot):
+        self.parent = None
+        self.shot = shot
 
 
 # Method to create a ball at a location.
 # 
+# TODO: Mofidy to accept ball object instead of a point.
+# TODO: No longer needs to return shape object
 def create_ball_at_location(point, space):
     body = pymunk.Body(10, 100)
     body.position = point[0], point[1]
     shape = pymunk.Circle(body, 15)
-    shape.friction = 0
+    # shape.friction = 0
+    # shape.ball_object = Ball()
     shape.collision_type = 2
     shape.elasticity = 1
     shape.body = body
@@ -45,6 +81,70 @@ def create_pockets(walls, space):
 
 
 
+# ----------- SHOT EVALUTION -------------------------------------------
+#   
+#
+# CREATE TABLE INITIAL STATE
+# PARAMETERS: Ball Locations, array of size 16 (required) of pymunk.Shape
+#             Pygame space obejct
+# RETURNS:    Nothing
+# ASSUMES:    
+# def create_initial_state()
+
+
+# EVALUATE ALL POSSIBLE SHOTS (FOR A GIVEN TABLE STATE)
+# PARAMETERS: Table object
+#             Pygame space obejct
+# RETURNS:    Array of successful shots
+# ASSUMES:
+# 
+# Define search space, angle and strength
+# Loop through all search options:(double for loop of possible strength and angle possibilites)
+#       Create new state of balls (create_initial_state) (or optionally, create a deep copy and pass to evaluate shot)
+#       Call Evaluate shot
+#       If shot made:
+#           Add to array of successful shots
+# Return array of successful shots to search tree algorithm
+def evaluate_all_possible_shots(table: Table, space: pygame.Space):
+    pass
+    
+def MOCKED_evaluate_all_possbile_shots(table:Table, space: pygame.Space):
+    pass
+# EVALUATE SINGLE SHOT
+# PARAMETERS: get_shot_diffulty (bool), space, force, angle, array of balls
+# ASSUMES:
+# RETURNS: shot object (if shot not made, return None)
+# ---- BEGIN SIMULATION ----
+# Possibly keep track of ball and wall bounces
+# If ball made, set shot flag to to true
+# ---- END SIMULATION ----
+# If shot made flag, create Shot object, return shot object
+
+# GET SHOT DIFFICULTY
+# PARAMETERS: 
+# ASSUMES:
+# REQUIRES:
+# ----------- SHOT EVALUTION -------------------------------------------
+
+
+# -------------- SEARCH TREE -------------------------------------------
+# Tree of Shots
+# Obtain initial positions of all balls
+# Create Table node
+#
+
+# -------------- SEARCH TREE ------------------------------------------- 
+
+
+# step 1
+# step 2
+# step 3 - evaluate_all_possible_shots(table, space)
+# step 4 
+
+
+
+
+
 # The pool table will have six walls, layout will look like this
 #
 #   X----0----X----1----X
@@ -53,6 +153,8 @@ def create_pockets(walls, space):
 #   |                   |
 #   X----4----X----3----X
 # Scale is 1:10, cm to pixels
+#
+# Balls array is always assumed to be of size 16
 
 def main():
     pygame.init()
@@ -77,6 +179,7 @@ def main():
     # pockets: List[pymunk.Shape] =[]
     # pockets.append()
 
+    # TODO: Change to array of Ball objects
     # List contains all pool balls, index 0 is cue ball, other indices correspond to ball numbers
     balls: List[pymunk.Shape] = []
     balls.append(create_ball_at_location((500, 500), space))
@@ -88,6 +191,8 @@ def main():
 
     force_applied = False
     paused = True
+
+    # MODIFY GAME LOOP TO INTERFACE WITH METHODS
     while running:
 
         # Event loop
